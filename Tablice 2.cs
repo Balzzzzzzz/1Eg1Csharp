a5f5658bb302999a1f662fe0e9a8b1f638842b97
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-12-12 10:43:15 +0100

    Update Tablice 2.cs

--- Tablice 2.cs
+++ Tablice 2.cs
@@ -15,30 +15,23 @@
 
 
 // podaj sumę liczb parzystych i ilość liczb nieparzystych
-Random r =new Random();
+Random r = new Random();
 int[] T = new int[100];
 int suma = 0;
 int nieparz = 0;
 for (int i = 0; i < T.Length; i++)
 {
     T[i] = r.Next(10, 99);
-    T[i] = r.Next(10, 31);
-    if (i % 2 == 0)
-    {
-        Console.WriteLine(" ");
-        Console.WriteLine(suma += i);
-    }
-    else
-    {
-        Console.WriteLine(" ");
-        Console.WriteLine(nieparz += 1);
-    }
-
+    if (i % 2 == 0) suma += T[i];
+    else nieparz += 1;
 }
 for (int i = 0; i < T.Length; i++)
 {
     Console.Write(T[i] + " ");
 }
+ Console.WriteLine(" ");
+Console.WriteLine("Suma parzystych: " + suma);
+Console.WriteLine("Ilość nieparzystych: " + nieparz);
 
 
 // TABLICE 2-WYMIAROWE
