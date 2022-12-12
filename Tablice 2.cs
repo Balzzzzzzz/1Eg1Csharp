7c89087dde42526a138286ae21137f6c2efdf8a2
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2022-12-12 10:16:08 +0100

    Update Tablice 2.cs

--- Tablice 2.cs
+++ Tablice 2.cs
@@ -1,7 +1,44 @@
 // ROZGRZEWKA
 // Stwórz tablicę 100 losowych liczb 2-cyfrowych
-// podaj sumę liczb parzystych i ilość liczb nieparzystych
 
+      Random r =new Random();
+      int[] T = new int[100];
+      for (int i = 0; i < T.Length; i++)
+      {
+          T[i] = r.Next(10, 99);
+          T[i] = r.Next(10, 31);
+      }
+      for (int i = 0; i < T.Length; i++)
+      {
+          Console.Write(T[i] + " ");
+      }
+
+
+// podaj sumę liczb parzystych i ilość liczb nieparzystych
+Random r =new Random();
+int[] T = new int[100];
+int suma = 0;
+int nieparz = 0;
+for (int i = 0; i < T.Length; i++)
+{
+    T[i] = r.Next(10, 99);
+    T[i] = r.Next(10, 31);
+    if (i % 2 == 0)
+    {
+        Console.WriteLine(" ");
+        Console.WriteLine(suma += i);
+    }
+    else
+    {
+        Console.WriteLine(" ");
+        Console.WriteLine(nieparz += 1);
+    }
+
+}
+for (int i = 0; i < T.Length; i++)
+{
+    Console.Write(T[i] + " ");
+}
 
 
 // TABLICE 2-WYMIAROWE
