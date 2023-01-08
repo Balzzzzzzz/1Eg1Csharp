d6c0991f38adb618abdfa5eabe3b6604ce30abb5
Yevhenii Drobotun <114587123+Balzzzzzzz@users.noreply.github.com>
2023-01-08 18:13:31 +0100

    Create KartaPracy 3A.cs

---
+++ KartaPracy 3A.cs
@@ -0,0 +1,17 @@
+int n = int.Parse(Console.ReadLine());
+for (int i = 1; i <= n; i++)
+{
+    Console.Write("*")
+}*/
+
+/*
+int n = int.Parse(Console.ReadLine());
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"({i},{j})");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
